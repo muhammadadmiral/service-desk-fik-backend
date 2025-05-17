b4998644fc1e6c9444e78d8f532772587462c969
muhammadadmiral <admelar15@gmail.com>
2025-05-17 22:06:05 +0700

    Chore : fix the console.log on main.ts

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
 
   const port = parseInt(process.env.PORT, 10) || 3001;
   await app.listen(port, '0.0.0.0');
-  console.log(`BACKEND NYA JALAN NIH MEMEK LU NGENTOT`);
+  console.log(`Backend running on PORT 3001`);
 }
 
 bootstrap();
